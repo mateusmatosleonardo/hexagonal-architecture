da5ae8eaf80173119c276b6a28d4e08ea80e8227
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-08-02 23:31:47 -0300

    update fundamentals menu

--- src/app/menu/fundamentals-menu.ts
+++ src/app/menu/fundamentals-menu.ts
@@ -1,16 +1,26 @@
-import TerminalUtil from "@/utils/terminal-util";
-import { terminal } from "terminal-kit";
+import TerminalUtil from "@/utils/terminal-util"
+import polimorfismo from "../fundamentals/polimorfismo"
+import dip from "../fundamentals/dip"
 
 export default async function fundamentalsMenu() {
-  TerminalUtil.title("Fundamentos");
+  TerminalUtil.title("Fundamentos")
 
-  const reponse = await terminal.singleColumnMenu(["1. Polimorfismo", "Voltar"])
-    .promise;
+  const [index] = await TerminalUtil.menu([
+    "1. Polimorfismo",
+    "2. DIP",
+    "Voltar"
+  ])
 
-  switch (reponse.selectedIndex) {
+  switch (index) {
+    case 0:
+      await polimorfismo()
+      break
     case 1:
-      return;
+      await dip()
+      break
+    default:
+      return
   }
 
-  await fundamentalsMenu();
+  await fundamentalsMenu()
 }
