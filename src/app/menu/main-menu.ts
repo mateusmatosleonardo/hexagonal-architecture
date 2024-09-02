ea264d375993cdc744ff55f6372f8a07566de201
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-09-02 16:13:57 -0300

    update main menu

--- src/app/menu/main-menu.ts
+++ src/app/menu/main-menu.ts
@@ -1,16 +1,24 @@
 import TerminalUtil from "@/utils/terminal-util"
 import fundamentalsMenu from "./fundamentals-menu"
+import userMenu from "./user-menu"
 
 export default async function mainMenu() {
   TerminalUtil.title("Menu principal")
 
-  const [index] = await TerminalUtil.menu(["1. Fundamentos", "Sair"])
+  const [index] = await TerminalUtil.menu([
+    "1. Fundamentos",
+    "2. Usuário",
+    "Sair"
+  ])
 
   switch (index) {
     case 0:
       await fundamentalsMenu()
       break
     case 1:
+      await userMenu()
+      break
+    default:
       process.exit(0)
   }
 
