4d7207add8ca07c881668bd9cc638229856aaecd
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-09-02 16:14:39 -0300

    feat: add fields in save user use case

--- src/app/user/save-user.ts
+++ src/app/user/save-user.ts
@@ -1,7 +1,20 @@
 import TerminalUtil from "@/utils/terminal-util"
+import User from "@/core/user/model/user"
+import SaveUser from "@/core/user/use-cases/save-user"
 
 export default async function saveUser() {
   TerminalUtil.title("Registrar usuário")
+
+  const id = await TerminalUtil.requiredField("Id: ")
+  const name = await TerminalUtil.requiredField("Nome: ")
+  const email = await TerminalUtil.requiredField("E-mail: ")
+  const password = await TerminalUtil.requiredField("Senha: ")
+
+  const user: User = { id, name, email, password }
+
+  await new SaveUser().execute(user)
+
+  TerminalUtil.success("Usuário registrado com sucesso")
 
   await TerminalUtil.waitEnter()
 }
--- src/core/user/use-cases/save-user.ts
+++ src/core/user/use-cases/save-user.ts
@@ -2,5 +2,8 @@ import UseCase from "@/core/shared/use-case"
 import User from "@/core/user/model/user"
 
 export default class SaveUser implements UseCase<User, void> {
-  async execute(user: User): Promise<void> {}
+  async execute(user: User): Promise<void> {
+    const criptPass = user.password.split("").reverse().join("")
+    console.log(`\n\n${criptPass}`)
+  }
 }
