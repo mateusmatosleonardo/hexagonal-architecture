99bac3d249e36414986d22afc57f637bba143ecc
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-11-04 13:40:37 -0400

    implements save user use case

--- src/app/user/save-user.ts
+++ src/app/user/save-user.ts
@@ -5,16 +5,24 @@ import SaveUser from "@/core/user/use-cases/save-user"
 export default async function saveUser() {
   TerminalUtil.title("Registrar usuário")
 
-  const id = await TerminalUtil.requiredField("Id: ")
+  // const id = await TerminalUtil.requiredField("Id: ")
   const name = await TerminalUtil.requiredField("Nome: ")
   const email = await TerminalUtil.requiredField("E-mail: ")
   const password = await TerminalUtil.requiredField("Senha: ")
 
-  const user: User = { id, name, email, password }
+  const user: User = { name, email, password }
 
   await new SaveUser().execute(user)
 
   TerminalUtil.success("Usuário registrado com sucesso")
 
   await TerminalUtil.waitEnter()
+
+  try {
+    await new SaveUser().execute(user)
+  } catch (e: any) {
+    TerminalUtil.error(e.message)
+  } finally {
+    await TerminalUtil.waitEnter()
+  }
 }
--- src/core/user/use-cases/save-user.ts
+++ src/core/user/use-cases/save-user.ts
@@ -1,9 +1,26 @@
 import UseCase from "@/core/shared/use-case"
+import Id from "@/core/shared/id"
 import User from "@/core/user/model/user"
+import errors from "@/core/shared/errors"
+import UserRepositoryInMemory from "../service/UserRepositoryInMemory"
 
 export default class SaveUser implements UseCase<User, void> {
   async execute(user: User): Promise<void> {
     const criptPass = user.password.split("").reverse().join("")
-    console.log(`\n\n${criptPass}`)
+    const repo = new UserRepositoryInMemory()
+
+    const existingUser = await repo.getByEmail(user.email)
+    if (existingUser) throw new Error(errors.USER_ALREADY_EXISTS)
+
+    const newUser: User = {
+      id: Id.generateHash(),
+      name: user.name,
+      email: user.email,
+      password: criptPass
+    }
+
+    await repo.insert(newUser)
+
+    console.log(`\n\n${JSON.stringify(newUser)}`)
   }
 }
