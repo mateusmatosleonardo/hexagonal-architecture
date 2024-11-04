4d4ae0c525c15e44af1e6ade633ec709f8ba2c98
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-11-04 16:16:49 -0400

    feat: inject crypto password in constructor save user use case

--- src/core/user/use-cases/save-user.ts
+++ src/core/user/use-cases/save-user.ts
@@ -3,11 +3,15 @@ import Id from "@/core/shared/id"
 import User from "@/core/user/model/user"
 import errors from "@/core/shared/errors"
 import UserRepositoryInMemory from "../service/UserRepositoryInMemory"
+import CryptoProvider from "../service/CryptoProvider"
 
-export default class SaveUser implements UseCase<User, void> {
+export default class SaveUserUseCase implements UseCase<User, void> {
+  constructor(private cryptoProvider: CryptoProvider) {}
+
   async execute(user: User): Promise<void> {
-    const criptPass = user.password.split("").reverse().join("")
     const repo = new UserRepositoryInMemory()
+
+    const criptPass = this.cryptoProvider.encrypt(user.password)
 
     const existingUser = await repo.getByEmail(user.email)
     if (existingUser) throw new Error(errors.USER_ALREADY_EXISTS)
